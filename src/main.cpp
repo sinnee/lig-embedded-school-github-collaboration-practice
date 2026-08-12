761a89aa2d179bc97ea0c08273ea9dcbe967a523
KimSeongSin <40656425+sinnee@users.noreply.github.com>
2026-08-12 11:49:22 +0900

    chore: prepare conflict exercise alpha

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-    Device device("LIG training device");
+    Device device("Team Alpha device");
     std::cout << device.status_message() << '\n';
     return 0;
 }
